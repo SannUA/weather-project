1af69eb8f177f0adda91bfb4a98025ed7962cc18
Aleksandr Baskakov <500vovavip@gmail.com>
2020-05-30 02:43:46 +0300

    Updates

---
+++ precache-manifest.35c08c20ecaa6e83e9c251825d5eff7b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39530afe78fd66e6e430bd6e81e849d3",
+    "url": "/weather-project/index.html"
+  },
+  {
+    "revision": "7b569a42580ba3959222",
+    "url": "/weather-project/static/css/main.fe26b460.chunk.css"
+  },
+  {
+    "revision": "31fde359987647956a23",
+    "url": "/weather-project/static/js/2.bd740d8d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weather-project/static/js/2.bd740d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b569a42580ba3959222",
+    "url": "/weather-project/static/js/main.6c0c16b6.chunk.js"
+  },
+  {
+    "revision": "ca9758298d16caf1bd7c",
+    "url": "/weather-project/static/js/runtime-main.065b3060.js"
+  },
+  {
+    "revision": "78222a47ed3d12a0a0ccc62294121458",
+    "url": "/weather-project/static/media/Kharkov.78222a47.jpg"
+  },
+  {
+    "revision": "19be900a3d64963e056649b54313adfb",
+    "url": "/weather-project/static/media/Kiev.19be900a.jpg"
+  },
+  {
+    "revision": "b2d944195e6e3dc18102118add9e2975",
+    "url": "/weather-project/static/media/Lviv.b2d94419.jpg"
+  },
+  {
+    "revision": "d93aa8282b75393ff8e97577b66294c5",
+    "url": "/weather-project/static/media/Odessa.d93aa828.jpg"
+  },
+  {
+    "revision": "ede75f3f4e7c6ec884e52ec7b6be953d",
+    "url": "/weather-project/static/media/Tornado_hurricane.ede75f3f.png"
+  },
+  {
+    "revision": "367489fddd1cd9ae9eda4b70fc768fd7",
+    "url": "/weather-project/static/media/drops.367489fd.png"
+  },
+  {
+    "revision": "ab86ac06332591d0af9762b4e7a0c7c6",
+    "url": "/weather-project/static/media/dust.ab86ac06.png"
+  },
+  {
+    "revision": "1a3b1593dc9353de7cca0510debd79ee",
+    "url": "/weather-project/static/media/few_clouds.1a3b1593.png"
+  },
+  {
+    "revision": "965574c37e807acd872f402526a6dc49",
+    "url": "/weather-project/static/media/freezing_rain.965574c3.png"
+  },
+  {
+    "revision": "dd4ed0fc9f3d70fe159f2a2dc79aa969",
+    "url": "/weather-project/static/media/medium_clouds.dd4ed0fc.png"
+  },
+  {
+    "revision": "1127f101f2675f7b6ee9b7ee4624c09c",
+    "url": "/weather-project/static/media/mist.1127f101.png"
+  },
+  {
+    "revision": "f01eaa6cc167be8c3d15dafbbea2d17a",
+    "url": "/weather-project/static/media/rain_heavy.f01eaa6c.png"
+  },
+  {
+    "revision": "07affcde24d2a727ac88435a4d1b2a55",
+    "url": "/weather-project/static/media/rain_light.07affcde.png"
+  },
+  {
+    "revision": "0c16f496981859d0fcba2f0c79b7aed7",
+    "url": "/weather-project/static/media/rain_very_heavy.0c16f496.png"
+  },
+  {
+    "revision": "4bd2e52a731e33d9041352dca121ec94",
+    "url": "/weather-project/static/media/snow.4bd2e52a.png"
+  },
+  {
+    "revision": "ae22d7973e9c2e33954afc51d344673e",
+    "url": "/weather-project/static/media/sun.ae22d797.png"
+  },
+  {
+    "revision": "e3e30ba65f6a28474a6c1ddc84198fa8",
+    "url": "/weather-project/static/media/sun1.e3e30ba6.png"
+  },
+  {
+    "revision": "762a3667aa1b754c4c60319d8f55bee4",
+    "url": "/weather-project/static/media/temp.762a3667.png"
+  },
+  {
+    "revision": "1b53fb217dc2ceaed980d3b114137dc0",
+    "url": "/weather-project/static/media/thunderstorm.1b53fb21.png"
+  },
+  {
+    "revision": "ad47f83cafb7345003ed1f8f7b23e525",
+    "url": "/weather-project/static/media/very_clouds.ad47f83c.png"
+  },
+  {
+    "revision": "839a9df80ffa6ee87b9d2282a4c0997f",
+    "url": "/weather-project/static/media/wind.839a9df8.png"
+  }
+]);
