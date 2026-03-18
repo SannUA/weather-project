a48b73a50bb1bcbc033812896094283722f78c40
Oleksandr Baskakov <baskakov@dolph.in>
2026-03-18 21:34:41 +0100

    fix api call

--- src/Components/Forcast/TodayForecast/TodayForecast.js
+++ src/Components/Forcast/TodayForecast/TodayForecast.js
@@ -15,7 +15,7 @@ class TodayForecast extends React.Component {
     }
 
     componentDidMount() {
-      fetch('https://api.openweathermap.org/data/2.5/onecall?lat=50.4333&lon=30.5167&units=metric&exclude=daily,hourly,minutely&appid=6fe5d797611fe6614d7e1fe142694059').then(res => res.json()).then(
+      fetch('https://api.openweathermap.org/data/2.5/weather?lat=50.4333&lon=30.5167&units=metric&exclude=daily,hourly,minutely&appid=6fe5d797611fe6614d7e1fe142694059').then(res => res.json()).then(
         (result) => {
           this.setState({
             isLoaded: true,
@@ -33,10 +33,10 @@ class TodayForecast extends React.Component {
 
       componentDidUpdate(prevProps) {
         if (this.props.ChosenTown !== prevProps.ChosenTown) {
-          const urlKiev = 'https://api.openweathermap.org/data/2.5/onecall?lat=50.4333&lon=30.5167&units=metric&exclude=daily,hourly,minutely&appid=6fe5d797611fe6614d7e1fe142694059';
-          const urlKharkov = 'https://api.openweathermap.org/data/2.5/onecall?lat=50&lon=36.25&units=metric&exclude=daily,hourly,minutely&appid=6fe5d797611fe6614d7e1fe142694059';
-          const urlOdessa = 'https://api.openweathermap.org/data/2.5/onecall?lat=46.4775&lon=30.7326&units=metric&exclude=daily,hourly,minutely&appid=6fe5d797611fe6614d7e1fe142694059';
-          const urlLviv = 'https://api.openweathermap.org/data/2.5/onecall?lat=49.8383&lon=24.0232&units=metric&exclude=daily,hourly,minutely&appid=6fe5d797611fe6614d7e1fe142694059';
+          const urlKiev = 'https://api.openweathermap.org/data/2.5/weather?lat=50.4333&lon=30.5167&units=metric&exclude=daily,hourly,minutely&appid=6fe5d797611fe6614d7e1fe142694059';
+          const urlKharkov = 'https://api.openweathermap.org/data/2.5/weather?lat=50&lon=36.25&units=metric&exclude=daily,hourly,minutely&appid=6fe5d797611fe6614d7e1fe142694059';
+          const urlOdessa = 'https://api.openweathermap.org/data/2.5/weather?lat=46.4775&lon=30.7326&units=metric&exclude=daily,hourly,minutely&appid=6fe5d797611fe6614d7e1fe142694059';
+          const urlLviv = 'https://api.openweathermap.org/data/2.5/weather?lat=49.8383&lon=24.0232&units=metric&exclude=daily,hourly,minutely&appid=6fe5d797611fe6614d7e1fe142694059';
           let url;
           switch (this.props.ChosenTown) {
             case('Kiev'): url = urlKiev;
